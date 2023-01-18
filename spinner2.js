9a9173a3702e968cffd9e1bbbff2856d4d7435c9
JunseongKim0105 <juns.kim0105@gmail.com>
2023-01-17 20:34:11 -0500

    spinner-refactoring

---
+++ spinner2.js
@@ -0,0 +1,40 @@
+let delay = 100;
+const clock = '\r|\r/\r-\r\\\r|\r/\r-\r\\\r|';
+
+for (const direction of clock) {
+  setTimeout(() => {
+    process.stdout.write(direction);
+  }, delay);
+  delay = delay + 300;
+}
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 700);
+
+// setTimeout(() => {
+//   process.stdout.write('\r| ');
+// }, 900);
+// setTimeout(() => {
+//   process.stdout.write('\r/  ');
+// }, 1100);
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1300);
+// setTimeout(() => {
+//   process.stdout.write('\r\\   ');
+// }, 1500);
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 1700);
